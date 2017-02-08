e135d2ddeb73cc3e8566dea60d634a0517d7bf76
EdwinTh <edwinthoen@gmail.com>
2017-02-08 09:34:20 +0100

    ifelse blog

--- blog_helpers.R
+++ blog_helpers.R
@@ -3,8 +3,8 @@ run_jekyll <- function() {
   servr::jekyll(dir = '/Users/edwinthoen/Documents/Site',
                 input = '_source',
                 output = '_posts',
-                serve = FALSE,
+                serve = TRUE,
                 command = 'bundle exec jekyll build')
 }
 
-
+run_jekyll()
