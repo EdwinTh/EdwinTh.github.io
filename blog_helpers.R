e275e17a05461b50c26d982c9a65376c47d6a5b1
EdwinTh <edwinthoen@gmail.com>
2017-07-20 19:19:02 +0200

    publishing check id vars

--- blog_helpers.R
+++ blog_helpers.R
@@ -3,7 +3,7 @@ run_jekyll <- function() {
   servr::jekyll(dir = '/Users/edwinthoen/Documents/Site',
                 input = '_source',
                 output = '_posts',
-                serve = TRUE,
+                # serve = TRUE,
                 command = 'bundle exec jekyll build')
 }
 
